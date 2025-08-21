46bea8dfed38d7794804515d6d68f16da460460a
SHANKET KUMAR <sraviket237@gmail.com>
2025-08-21 17:33:28 +0530

    Added Gunicorn to requirements and fixed deployment issues

---
+++ credimate/credimate/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for credimate project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "credimate.settings")
+
+application = get_wsgi_application()
